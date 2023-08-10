4b97dccbc110cc765ed74d89708d5c2cf858276e
Alisha Khan <120572421+AlishaKhan-07@users.noreply.github.com>
2023-08-10 18:11:24 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,14 @@
+let firebaseConfig = {
+  apiKey: "AIzaSyDEDdZQc3yFmHoKxY9pH95lJ-svCqMmFiI",
+  authDomain: "content-management-tool-8d81a.firebaseapp.com",
+  projectId: "content-management-tool-8d81a",
+  storageBucket: "content-management-tool-8d81a.appspot.com",
+  messagingSenderId: "629857308797",
+  appId: "1:629857308797:web:b81d59807973f9e2efe114"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+
+let db = firebase.firestore();
